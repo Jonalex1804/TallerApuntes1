37003adcffb3b8702e190cd66d6987a6f26c21ef
Jonalex1804 <105876418+Jonalex1804@users.noreply.github.com>
2024-05-23 21:53:39 -0500

    Agregar archivos de proyecto.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+
+        public string Nombre => "Jonathan Granja";
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://github.com/Jonalex1804";
+        public string Message => "Hola mi nombre es Jonathan pero todos me dicen Jonalex, me gustan los videojuegos, leer y tambien practicar en mis tiempos libres";
+    }
+}
